2896e000c6f50fc41edfbcb80d928f959757675b
Saurav Sabu <saurav.sabu9@gmail.com>
2022-12-28 00:03:13 +0530

    add courses to the home page

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ with col2:
     st.write("""
     In late December 2022, an extratropical bomb cyclone brought blizzard conditions and winter storms to much of Canada and the United States, killing 72 people, causing vehicle pileups and road closures, and canceling or delaying more than 10,000 flights during the busy Christmas travel season. The storm was unofficially named Winter Storm Elliott by The Weather Channel. The National Weather Service in Buffalo, New York predicted this to be a "once-in-a-generation storm".[5][6][7]   
     """)
+
+
+st.header("Courses Offered")
+
+st.selectbox("Enter choice",["Data Science","Data Analyst","Data Engineering"])
