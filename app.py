0b0aacf137becde3b558af2013e7d6d8d5d52226
Saurav Sabu <saurav.sabu9@gmail.com>
2022-12-27 23:36:11 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Saurav Sabu Portfolio Website")
+
+col1,col2 = st.columns(2)
+
+with col1:
+    st.image("dog.jpg")
+with col2:
+    st.write("""
+    In late December 2022, an extratropical bomb cyclone brought blizzard conditions and winter storms to much of Canada and the United States, killing 72 people, causing vehicle pileups and road closures, and canceling or delaying more than 10,000 flights during the busy Christmas travel season. The storm was unofficially named Winter Storm Elliott by The Weather Channel. The National Weather Service in Buffalo, New York predicted this to be a "once-in-a-generation storm".[5][6][7]   
+    """)
